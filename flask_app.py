3c2be63d802504c392a4f8eb0459df99ec02942d
Adhan Razzaque <adhan.razzaque@gmail.com>
2018-12-05 22:42:13 +0000

    Updated html of all pages, fixed links and js on index, added 404

---
+++ flask_app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def main():
+    return render_template("index.html")
+
+@app.route('/charts', methods=['GET'])
+def charts():
+    return render_template('charts.html')
+
+@app.route('/blank', methods=['GET'])
+def blank():
+    return render_template('blank.html')
+
+@app.route('/forgot-password', methods=['GET'])
+def forgot_password():
+    return render_template('forgot-password.html')
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET'])
+def register():
+    return render_template('register.html')
+
+@app.route('/tables', methods=['GET'])
+def tables():
+    return render_template('tables.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run()
