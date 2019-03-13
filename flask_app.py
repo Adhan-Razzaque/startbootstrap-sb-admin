4c0fb845f304d936642ed8880d1925bf5fa9c5da
Adhan Razzaque <adhan.razzaque@gmail.com>
2019-03-13 21:44:31 +0000

    Tweaked login framework, must work on imports

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,83 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, render_template, url_for, flash, request, is_safe_url
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import LoginManager, LoginForm
 # from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+
 db = SQLAlchemy()
 
-#...
+
+@login_manager.user_loader
+def user_loader(user_id):
+    """Given *user_id*, return the associated User object.
+
+    :param unicode user_id: user_id (email) user to retrieve
+
+    """
+    return User.query.get(user_id)
+
+@app.route('/', methods=['GET'])
+def main():
+    return render_template("index.html")
+
+@app.route('/charts', methods=['GET'])
+def charts():
+    return render_template('charts.html')
+
+@app.route('/blank', methods=['GET'])
+def blank():
+    return render_template('blank.html')
+
+@app.route('/forgot-password', methods=['GET'])
+def forgot_password():
+    return render_template('forgot-password.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Here we use a class of some kind to represent and validate our
+    # client-side form data. For example, WTForms is a library that will
+    # handle this for us, and we use a custom LoginForm to validate.
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        login_user(user)
+
+        flash('Logged in successfully.')
+
+        next = request.args.get('next')
+        # is_safe_url should check if the url is safe for redirects.
+        # See http://flask.pocoo.org/snippets/62/ for an example.
+        if not is_safe_url(next):
+            return abort(400)
+
+        return redirect(next or url_for('index'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/register', methods=['GET'])
+def register():
+    return render_template('register.html')
+
+@app.route('/tables', methods=['GET'])
+def tables():
+    return render_template('tables.html')
+
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template("index.html")
+
+@app.route('/contactme', methods=['GET', 'POST'])
+def contactme():
+    return render_template("contact.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 class User(db.Model):
     """An admin user capable of viewing reports.
@@ -37,81 +108,7 @@ class User(db.Model):
         """False, as anonymous users aren't supported."""
         return False
 
-@login_manager.user_loader
-def user_loader(user_id):
-    """Given *user_id*, return the associated User object.
 
-    :param unicode user_id: user_id (email) user to retrieve
-
-    """
-    return User.query.get(user_id)
-
-@app.route('/', methods=['GET'])
-def main():
-    return render_template("index.html")
-
-@app.route('/charts', methods=['GET'])
-def charts():
-    return render_template('charts.html')
-
-@app.route('/blank', methods=['GET'])
-def blank():
-    return render_template('blank.html')
-
-@app.route('/forgot-password', methods=['GET'])
-def forgot_password():
-    return render_template('forgot-password.html')
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    """For GET requests, display the login form.
-    For POSTS, login the current user by processing the form.
-
-    """
-    print(db)
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.get(form.email.data)
-        if user:
-            if bcrypt.check_password_hash(user.password, form.password.data):
-                user.authenticated = True
-                db.session.add(user)
-                db.session.commit()
-                login_user(user, remember=True)
-                return redirect(url_for("bull.reports"))
-    return render_template("login.html", form=form)
-
-@app.route("/logout", methods=["GET"])
-@login_required
-def logout():
-    """Logout the current user."""
-    user = current_user
-    user.authenticated = False
-    db.session.add(user)
-    db.session.commit()
-    logout_user()
-    return render_template("logout.html")
-
-
-@app.route('/register', methods=['GET'])
-def register():
-    return render_template('register.html')
-
-@app.route('/tables', methods=['GET'])
-def tables():
-    return render_template('tables.html')
-
-@app.route('/home', methods=['GET'])
-def home():
-    return render_template("index.html")
-
-@app.route('/contactme', methods=['GET', 'POST'])
-def contactme():
-    return render_template("contact.html")
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
