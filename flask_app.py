83d90682cd21e742ae997ea189f356e9ef87d5b2
Adhan Razzaque <adhan.razzaque@gmail.com>
2019-03-27 21:44:40 +0000

    Few small additions to the login manager

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, url_for, flash, request, is_safe_url
+from flask import Flask, render_template, url_for, flash, request, is_safe_url, redirect, abort
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, LoginForm
+from flask_login import LoginManager, LoginForm, login_user
 # from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
+
+
+#key for login manager
+app.secret_key = b'ZcTLsnInOiDajLQax0NekA9F'
 
 login_manager = LoginManager()
 
@@ -10,6 +14,43 @@ login_manager.init_app(app)
 
 db = SQLAlchemy()
 
+# class User(db.Model):
+#     # ...
+
+#     def set_password(self, password):
+#         self.password_hash = generate_password_hash(password)
+
+#     def check_password(self, password):
+#         return check_password_hash(self.password_hash, password)
+
+class User(db.Model):
+    """An admin user capable of viewing reports.
+
+    :param str email: email address of user
+    :param str password: encrypted password for the user
+
+    """
+    __tablename__ = 'user'
+
+    email = db.Column(db.String, primary_key=True)
+    password = db.Column(db.String)
+    authenticated = db.Column(db.Boolean, default=False)
+
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.email
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
 
 @login_manager.user_loader
 def user_loader(user_id):
@@ -78,38 +119,6 @@ def contactme():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
-class User(db.Model):
-    """An admin user capable of viewing reports.
-
-    :param str email: email address of user
-    :param str password: encrypted password for the user
-
-    """
-    __tablename__ = 'user'
-
-    email = db.Column(db.String, primary_key=True)
-    password = db.Column(db.String)
-    authenticated = db.Column(db.Boolean, default=False)
-
-    def is_active(self):
-        """True, as all users are active."""
-        return True
-
-    def get_id(self):
-        """Return the email address to satisfy Flask-Login's requirements."""
-        return self.email
-
-    def is_authenticated(self):
-        """Return True if the user is authenticated."""
-        return self.authenticated
-
-    def is_anonymous(self):
-        """False, as anonymous users aren't supported."""
-        return False
-
-
-
 
 if __name__ == '__main__':
     app.run()
