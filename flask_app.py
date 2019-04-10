bbba2923ba2fbbd1a6bbdaeea9dd094a7de261f9
Adhan Razzaque <adhan.razzaque@gmail.com>
2019-04-10 21:41:53 +0000

    Adjusted login, fixed HTML on login, need to check imports

--- flask_app.py
+++ flask_app.py
@@ -1,65 +1,55 @@
-from flask import Flask, render_template, url_for, flash, request, is_safe_url, redirect, abort
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, LoginForm, login_user
-# from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, abort, redirect, Response, url_for
+from flask_login import LoginManager, login_required, UserMixin, login_user
+
 app = Flask(__name__)
 
-
-#key for login manager
-app.secret_key = b'ZcTLsnInOiDajLQax0NekA9F'
-
+#https://github.com/shekhargulati/flask-login-example/blob/master/flask-login-example.py
+#https://github.com/shekhargulati/flask-login-example/issues
+app.config['SECRET_KEY'] = 'a2BxpjeRLJBOyTWluc0NqA'
 login_manager = LoginManager()
-
+login_manager.login_view = "login"
 login_manager.init_app(app)
 
-db = SQLAlchemy()
-
-# class User(db.Model):
-#     # ...
-
-#     def set_password(self, password):
-#         self.password_hash = generate_password_hash(password)
-
-#     def check_password(self, password):
-#         return check_password_hash(self.password_hash, password)
-
-class User(db.Model):
-    """An admin user capable of viewing reports.
-
-    :param str email: email address of user
-    :param str password: encrypted password for the user
-
-    """
-    __tablename__ = 'user'
-
-    email = db.Column(db.String, primary_key=True)
-    password = db.Column(db.String)
-    authenticated = db.Column(db.Boolean, default=False)
-
-    def is_active(self):
-        """True, as all users are active."""
-        return True
+class User(UserMixin):
+    def __init__(self , username , password , id , active=True):
+        self.id = id
+        self.username = username
+        self.password = password
+        self.active = active
 
     def get_id(self):
-        """Return the email address to satisfy Flask-Login's requirements."""
-        return self.email
+        return self.id
 
-    def is_authenticated(self):
-        """Return True if the user is authenticated."""
-        return self.authenticated
+    def is_active(self):
+        return self.active
 
-    def is_anonymous(self):
-        """False, as anonymous users aren't supported."""
-        return False
+    def get_auth_token(self):
+        return make_secure_token(self.username , key='a2BxpjeRLJBOyTWluc0NqA')
 
-@login_manager.user_loader
-def user_loader(user_id):
-    """Given *user_id*, return the associated User object.
+class UsersRepository:
 
-    :param unicode user_id: user_id (email) user to retrieve
+    def __init__(self):
+        self.users = dict()
+        self.users_id_dict = dict()
+        self.identifier = 0
 
-    """
-    return User.query.get(user_id)
+    def save_user(self, user):
+        self.users_id_dict.setdefault(user.id, user)
+        self.users.setdefault(user.username, user)
+
+    def get_user(self, username):
+        return self.users.get(username)
+
+    def get_user_by_id(self, userid):
+        return self.users_id_dict.get(userid)
+
+    def next_index(self):
+        self.identifier +=1
+        return self.identifier
+
+users_repository = UsersRepository()
+
+
 
 @app.route('/', methods=['GET'])
 def main():
@@ -77,34 +67,38 @@ def blank():
 def forgot_password():
     return render_template('forgot-password.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST']) #add POST method for login
 def login():
-    # Here we use a class of some kind to represent and validate our
-    # client-side form data. For example, WTForms is a library that will
-    # handle this for us, and we use a custom LoginForm to validate.
-    form = LoginForm()
-    if form.validate_on_submit():
-        # Login and validate the user.
-        # user should be an instance of your `User` class
-        login_user(user)
-
-        flash('Logged in successfully.')
-
-        next = request.args.get('next')
-        # is_safe_url should check if the url is safe for redirects.
-        # See http://flask.pocoo.org/snippets/62/ for an example.
-        if not is_safe_url(next):
-            return abort(400)
-
-        return redirect(next or url_for('index'))
-    return render_template('login.html', form=form)
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        remLog = request.form['remember-me']
+        registeredUser = users_repository.get_user(username)
+        print('Users '+ str(users_repository.users))
+        print('Register user %s , password %s' % (registeredUser.username, registeredUser.password))
+        if registeredUser != None and registeredUser.password == password:
+            print('Logged in..')
+            login_user(registeredUser, remember=remLog)
+            return redirect(url_for('home'))
+        else:
+            return abort(401)
+    else:
+        return render_template('login.html')
 
 
-@app.route('/register', methods=['GET'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        new_user = User(username , password , users_repository.next_index())
+        users_repository.save_user(new_user)
+        return Response("Registered Successfully")
+    else:
+        return render_template('register.html')
 
 @app.route('/tables', methods=['GET'])
+@login_required
 def tables():
     return render_template('tables.html')
 
@@ -119,6 +113,16 @@ def contactme():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+# handle login failed
+@app.errorhandler(401)
+def login_failure(e):
+    return Response('<p>Login failed</p>')
+
+# callback to reload the user object
+@login_manager.user_loader
+def load_user(userid):
+    return users_repository.get_user_by_id(userid)
 
 if __name__ == '__main__':
     app.run()
